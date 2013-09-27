213e5f6cf148bd4ca1b1fbbf8ca1ff80728cedfa
Jonathan Felch <jonathan.felch@gmail.com>
2013-09-26 18:13:13 -0600

    moved system installs from usr/local to opt

---
+++ dev-env.sh
@@ -0,0 +1,9 @@
+export JAVA_HOME=/opt/default
+export GROOVY_HOME=/opt/groovy/default
+export SCALA_HOME=/opt/scala/default
+
+export JDK_HOME=$JAVA_HOME
+export MONGODB_HOME=/opt/mongodb/default
+export ZEROMQ_HOME=/opt/zeromq/default
+
+export PATH=$JDK_HOME/bin:$SCALA_HOME/bin:$GROOVY_HOME/bin:$ZEROMQ_HOME/bin:$MONGODB/bin:$PATH

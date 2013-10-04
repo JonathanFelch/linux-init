4efa4c3a9167b22f9c136dc0cbd3b2c6be22e0e3
Jonathan Felch <jonathan.felch@gmail.com>
2013-10-03 21:10:48 -0600

    expansion and tweaks for git aliases

--- git-env.sh
+++ git-env.sh
@@ -12,19 +12,24 @@ st(){ sublime-text-2 $*
 }
  
  
-# git aliases
-alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-alias gst='git status'
-alias gl='git pull'
-alias gp='git push'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gb='git branch'
-alias gco='git checkout'
-alias gcob='git checkout -b'
-alias glog='git log'
-alias gd='git diff --color'
+# Xephyr
 alias xephyrstart='Xephyr -ac -screen 1280x1024 -br -reset -terminate 2> /dev/null :1 &'
+
+# git commamands simplified
+alias gst='git status'
+alias gco='git checkout'
+alias gci='git commit -v'
+alias gd='git diff --color'
+alias gcob='git checkout -b'
+alias grb='git rebase'
+alias gbr='git branch'
+alias gpl='git pull'
+alias gpu='git push'
+alias gad='git add -A'
+alias gmt='git mergetool'
+alias bdf='git diff'
+alias glg='git log --date-order --all --graph --format="%C(green)%h%Creset %C(yellow)%an%Creset %C(blue bold)%ar%Creset %C(red bold)%d%Creset%s"'
+alias glg2='git log --date-order --all --graph --name-status --format="%C(green)%H%Creset %C(yellow)%an%Creset %C(blue bold)%ar%Creset %C(red bold)%d%Creset%s"'
  
 # git functions: aliases cannot have args
 # alias gbd='git config branch.$0.description'
